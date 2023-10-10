492797262acd93692c5626e64b4471fd169990f1
sukesh immadisetty <sukeshjanu143@gmail.com>
2023-10-10 17:57:38 +0530

    Vendor Registration

--- accounts/models.py
+++ accounts/models.py
@@ -45,10 +45,10 @@ class UserManager(BaseUserManager):
     
 
 class User(AbstractBaseUser):
-    RESTAURANT = 1
+    VENDOR = 1
     CUSTOMER = 2
     ROLE_CHOICE = (
-        (RESTAURANT,'Restaurant'),
+        (VENDOR,'DISPENSARY'),
         (CUSTOMER,'Customer'),
     )
     first_name = models.CharField(max_length=50)
