cdd9cdfb6a4ccbf9cb20b85fbc76e86a8f569d72
sukesh immadisetty <sukeshjanu143@gmail.com>
2023-11-17 16:37:09 +0530

    Cart functionalites

--- marketplace/models.py
+++ marketplace/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from accounts.models import User
+from menu.models import Medicine_lobby
 # Create your models here.
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    medlist = models.ForeignKey(Medicine_lobby, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.user
--- marketplace/views.py
+++ marketplace/views.py
@@ -1,7 +1,10 @@
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, render
 from Medicine_Vendor.models import Vendor
-from menu.models import Category
+from marketplace.models import Cart
+from menu.models import Category, Medicine_lobby
 from django.db.models import Prefetch
+from .context_processors import get_cart_counter
 # Create your views here.
 
 def marketplace(request):
@@ -23,8 +26,69 @@ def vendor_detail(request, vendor_slug):
         )
     )
 
+    if request.user.is_authenticated:
+        cart_items = Cart.objects.filter(user=request.user)
+    else:
+        cart_items = None
     context = {
         'vendor':vendor,
         'categories':categories,
+        'cart_items':cart_items,
     }
     return render(request, 'marketplace/vendor_detail.html',context)
+
+
+
+
+
+
+
+
+def add_to_cart(request, med_id):
+    if request.user.is_authenticated:
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            try:
+                medlist = Medicine_lobby.objects.get(id=med_id)
+                try:
+                    chkCart=Cart.objects.get(user=request.user,medlist=medlist)
+                    #increase cart quantity
+                    chkCart.quantity += 1
+                    chkCart.save()
+                    return JsonResponse({'status':'Success','message':'increased cart quantity','cart_counter': get_cart_counter(request), 'qty':chkCart.quantity})
+                except:
+                    chkCart=Cart.objects.create(user=request.user, medlist=medlist, quantity=1)
+                    return JsonResponse({'status':'Success','message':'Added the med to cart','cart_counter': get_cart_counter(request), 'qty':chkCart.quantity})
+            except:
+                return JsonResponse({'status':'Failed','message':'This med does not exist'})
+        else:
+            return JsonResponse({'status':'Failed','message':'Invalid login'})
+    else:
+        return JsonResponse({'status':'login_required','message':'Please login to continue'})
+    
+
+def decrease_cart(request,med_id):
+    if request.user.is_authenticated:
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            try:
+                medlist = Medicine_lobby.objects.get(id=med_id)
+                try:
+                    chkCart=Cart.objects.get(user=request.user,medlist=medlist)
+                    if chkCart.quantity>1:
+                    #Decrease cart quantity
+                        chkCart.quantity -= 1
+                        chkCart.save()
+                    else:
+                        chkCart.delete()
+                        chkCart.quantity = 0
+                    return JsonResponse({'status':'Success','cart_counter': get_cart_counter(request), 'qty':chkCart.quantity})
+                except:
+                    return JsonResponse({'status':'Failed','message':'You do not have med to cart'})
+            except:
+                return JsonResponse({'status':'Failed','message':'This med does not exist'})
+        else:
+            return JsonResponse({'status':'Failed','message':'Invalid login'})
+    else:
+        return JsonResponse({'status':'login_required','message':'Please login to continue'})
+
+
+
